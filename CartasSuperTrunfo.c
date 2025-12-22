2910f47908dc068e9d4cfae593c315263280bb73
Faelsantos21 <rafaelwords@hotmail.com>
2025-12-22 13:43:22 +0000

    Desafio nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
+// Desafio Super Trunfo - Países nivel aventureiro.
+// Tema 1 - Cadastro das cartas + updade calculo de densidade + calculo area per capita.
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
@@ -12,7 +12,7 @@ int main() {
 
 char carta1[3],carta2[3], estado1[20], estado2[20], cidade1[20], cidade2[20], codigo1[30],codigo2[30];
 int populacao1,populacao2, pontosturisticos1, pontosturisticos2;
-float areakm1, areakm2 , pib1, pib2;
+float areakm1, areakm2 , pib1, pib2, densidadepopulacional1,densidadepopulacional2, pibporcapita1,pibporcapita2;
 
 //Esses códigos abaixo perdem informações da Carta 1
 
@@ -43,7 +43,7 @@ scanf("%i" , &pontosturisticos1 );
 
 //Esses códigos abaixo perdem informações da Carta 2
 
-     printf("Digite o numero da carta2\n");
+     printf("\nDigite o numero da carta2\n");
 scanf("%s" , carta2);
 
 printf("digite a letra correspondente ao estado \n");
@@ -67,14 +67,28 @@ scanf("%f" , &pib2);
 printf("Quantidade de pontos turisticos\n");
 scanf("%i" , &pontosturisticos2);
 
+// essas declarações de variaveis fazem o calculo da densidade e pibprcapita.
+densidadepopulacional1 = populacao1 / areakm1;
+pibporcapita1 = pib1 / populacao1;
+
+densidadepopulacional2 = populacao2 / areakm2;
+pibporcapita2 = pib2 / populacao2;
+
 //Esse código imprime as informações da carta1 e carta2 fornecidas pelo usuário
 
-printf( "Carta:%s\nEstado %s\nCodigo %s\nCidade %s\nPopulação:%d\nÁrea %f\nPib %f\nPontos Turisticos %i\nCarta: %s\nEstado %s\nCodigo %s\nCidade %s\nPopulação: %i\nÁrea %f\nPib %f\nPontos Turisticos %i\n" ,
-    carta1, estado1, codigo1, cidade1, populacao1, areakm1, pib1, pontosturisticos1,carta2, estado2, codigo2, cidade2, populacao2, areakm2, pib2, pontosturisticos2);
+printf(
+"Carta: %s\nEstado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turisticos: %i\nDensidade Populacional: %.2f\nPIB per capita: %.2f\n\n"
+
+"Carta: %s\nEstado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turisticos: %i\nDensidade Populacional: %.2f\nPIB per capita: %.2f\n",
+
+carta1, estado1, codigo1, cidade1,populacao1, areakm1, pib1, pontosturisticos1,
+densidadepopulacional1, pibporcapita1,
+
+carta2, estado2, codigo2, cidade2,populacao2, areakm2, pib2, pontosturisticos2,
+densidadepopulacional2, pibporcapita2);
 
      
 return 0;
-
 
 }
 
